0383d2a9ef8bfd82747ec01892cd00bc77c53175
MadhanArts <madhanartsmadhan@gmail.com>
2021-05-24 23:07:54 +0530

    24-05-2021 Navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "./NavBar.css";
+
+function NavBar() {
+    return (
+        <nav className='nav-bar'>
+            <Link to='/'>
+                <div className='logo'>
+                    <img src='' alt='' />
+                    <div className='logo-text'>Madhan</div>
+                </div>
+            </Link>
+            <div className='menu-items'>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/projects'>Projects</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
