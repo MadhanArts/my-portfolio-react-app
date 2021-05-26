8ebc0015f0ec032037b338e34c84935c458d6cc2
MadhanArts <madhanartsmadhan@gmail.com>
2021-05-26 23:25:45 +0530

    26-05-2021 Projects Section

---
+++ src/components/Projects/Project.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Project({project_image, title, description}) {
+    return (
+        <div className='project'>
+            <img src={project_image} alt='project images' />
+            <div>
+                <h4 className='project-title'>{title}</h4>
+                <p className='project-description'>{description}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Project;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import Project from "./Project";
+import "./Projects.css";
 
 function Projects() {
-    return <div>Projects</div>;
+    return (
+        <div className='projects-container'>
+            <div className='section app-development-projects'>
+                <h3>App Development</h3>
+                <div className='projects'>
+                    <Project
+                        title='Voice Recorder App'
+                        description='Its a simple voice recorder app, where you can
+                    record your crystal clear voice.'
+                    />
+
+                    <Project
+                        title='Notes App'
+                        description='This is the note taking app with voice recording
+                    feature. In this app, we can take text and voice
+                    recordings notes. We can also create a checklist.'
+                    />
+
+                    <Project
+                        title='Music Player App'
+                        description='This is the Music Player app, where you can play
+                    songs, voice recordings or any kind of audio files.'
+                    />
+                </div>
+            </div>
+            <div className='section web-development-projects'>
+                <h3>Web Development</h3>
+                <div className='projects'>
+                    <Project
+                        title='CRUD Operations on MongoDB'
+                        description='This is a website, which is the demo for Create, Read, 
+                    Update and Delete operation on MongoDB database using mongoose. 
+                    Here I connected React and Express using Axios API call.'
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Projects;
