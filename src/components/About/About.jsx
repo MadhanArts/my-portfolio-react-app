f4a0e1349b18f7b0351baeacb849469637e67bd1
MadhanArts <madhanartsmadhan@gmail.com>
2021-05-27 16:35:52 +0530

    27-05-2021 About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-    return <div>About</div>;
+    return (
+        <div className='about-container'>
+            <div className='about section'>
+                <h3>About Me</h3>
+                <p>
+                    I am Madhan, loves learning new things everyday. I have good
+                    experience on app development, problem solving, Editing, and
+                    photography. I have done projects on app development. Learnt
+                    programming languages like C, Java, Python and Javascript. I
+                    was interested on Maths, Science and Computer. Believing
+                    “Education is not just getting marks, but learning and
+                    understanding the concepts matters” and following the same.
+                </p>
+            </div>
+            <div className='education section'>
+                <h3>Education</h3>
+                <h4>College</h4>
+                <ul>
+                    <li>
+                        <p>
+                            Currently pursuing B.E (Computer Science and
+                            Engineering) at Saveetha Engineering College under
+                            Anna University, Chennai. Upto 5th semester, C.G.P.A
+                            is 85.82. Year of passing is 2022.
+                        </p>
+                    </li>
+                </ul>
+
+                <h4>Schools</h4>
+                <ul>
+                    <li>
+                        <p>
+                            Studied XII at Seventh Day Adventist Matriculation
+                            Higher Secondary School under Directorate of
+                            Government Examinations, Tamil Nadu and secured
+                            85.08 % in the year 2018.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Studied X at Seventh Day Adventist Matriculation
+                            Higher Secondary School under Directorate of
+                            Government Examinations, Tamil Nadu and secured 90.4
+                            % in the year 2016.
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default About;
